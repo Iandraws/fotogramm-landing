e9f8d45aefdb666bbc5939c40180b04db9802c9d
Lawrence Nseir <lawrence.nseir@gmail.com>
2024-11-07 20:30:43 +0100

    refactor: remove console.log

--- fotogram/src/helpers/translate.js
+++ fotogram/src/helpers/translate.js
@@ -3,7 +3,6 @@ import wording from '../constants/wording';
 const lang = localStorage.getItem('lang') || 'en';
 
 function translate(key) {
-  console.log(lang)
   return wording[key] ? wording[key][lang] || wording[key]['en'] : key;
 }
 
