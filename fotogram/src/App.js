bb709872146a24d6fcd873d97f3ab9598b8145fd
Lawrence Nseir <lawrence.nseir@gmail.com>
2024-11-25 19:52:20 +0100

    feat: add seo

--- fotogram/src/App.js
+++ fotogram/src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import ContactUs from './components/ContactUs';
@@ -13,13 +12,14 @@ import SignUpForm from './components/SignUpForm';
 import TermsAndConditionsDe from './components/TermsAndConditionsDe';
 import TermsAndConditionsEn from './components/TermsAndConditionsEn';
 import WhoWeAre from './components/WhoWeAre';
-
 import ImprintDe from './components/ImprintDe';
 import ImprintEn from './components/ImprintEn';
 import PrivacyPolicyDe from './components/PrivacyPolicyDe';
 import PrivacyPolicyEn from './components/PrivacyPolicyEn';
-
+import { HelmetProvider } from 'react-helmet-async';
 import LogRocket from 'logrocket';
+
+// Initialize LogRocket for monitoring
 LogRocket.init('edvm0h/fotogram-landing-page');
 
 function App() {
@@ -78,70 +78,128 @@ function App() {
     },
   };
 
+  const metaDescriptions = {
+    '/': {
+      en: 'Welcome to Fotogram Technology, the ultimate platform for your needs.',
+      de: 'Willkommen bei Fotogram Technology, der ultimativen Plattform für Ihre Bedürfnisse.',
+    },
+    '/pricing': {
+      en: 'Explore Fotogram Technology pricing plans tailored to your needs.',
+      de: 'Entdecken Sie die auf Ihre Bedürfnisse zugeschnittenen Preismodelle von Fotogram Technology.',
+    },
+    '/signup': {
+      en: 'Sign up to start your journey with Fotogram Technology.',
+      de: 'Melden Sie sich an, um Ihre Reise mit Fotogram Technology zu beginnen.',
+    },
+    '/faq': {
+      en: 'Find answers to frequently asked questions about Fotogram Technology.',
+      de: 'Finden Sie Antworten auf häufig gestellte Fragen zu Fotogram Technology.',
+    },
+    '/why-fotogram': {
+      en: 'Discover why Fotogram Technology is the best choice for you.',
+      de: 'Erfahren Sie, warum Fotogram Technology die beste Wahl für Sie ist.',
+    },
+    '/our-story': {
+      en: 'Learn about the journey and vision of Fotogram Technology.',
+      de: 'Erfahren Sie mehr über die Reise und Vision von Fotogram Technology.',
+    },
+    '/get-in-touch': {
+      en: 'Contact Fotogram Technology for support and inquiries.',
+      de: 'Kontaktieren Sie Fotogram Technology für Unterstützung und Anfragen.',
+    },
+    '/en/terms-and-conditions': {
+      en: 'Read the terms and conditions for using Fotogram Technology.',
+      de: 'Lesen Sie die Allgemeinen Geschäftsbedingungen für die Nutzung von Fotogram Technology.',
+    },
+    '/de/terms-and-conditions': {
+      en: 'Read the terms and conditions for using Fotogram Technology.',
+      de: 'Lesen Sie die Allgemeinen Geschäftsbedingungen für die Nutzung von Fotogram Technology.',
+    },
+    '/en/imprint': {
+      en: 'Legal imprint for Fotogram Technology in English.',
+      de: 'Rechtliches Impressum für Fotogram Technology auf Englisch.',
+    },
+    '/de/imprint': {
+      en: 'Legal imprint for Fotogram Technology in German.',
+      de: 'Rechtliches Impressum für Fotogram Technology auf Deutsch.',
+    },
+    '/en/privacy-policy': {
+      en: "Learn about Fotogram Technology's privacy practices.",
+      de: 'Erfahren Sie mehr über die Datenschutzpraktiken von Fotogram Technology.',
+    },
+    '/de/privacy-policy': {
+      en: "Learn about Fotogram Technology's privacy practices.",
+      de: 'Erfahren Sie mehr über die Datenschutzpraktiken von Fotogram Technology.',
+    },
+  };
+
   return (
-    <Router>
-      <ScrollToTop titles={titles} />
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={<HeroSection />}
+    <HelmetProvider>
+      <Router>
+        <ScrollToTop
+          titles={titles}
+          metaDescriptions={metaDescriptions}
+          defaultTitle="Fotogram Technology"
+          defaultDescription="Discover Fotogram Technology for all your needs."
         />
-        <Route
-          path="/pricing"
-          element={<PricingTable />}
-        />
-        <Route
-          path="/signup"
-          element={<SignUpForm />}
-        />
-        <Route
-          path="/faq"
-          element={<Help />}
-        />
-        <Route
-          path="/why-fotogram"
-          element={<HowItWorks />}
-        />
-        <Route
-          path="/our-story"
-          element={<WhoWeAre />}
-        />
-        <Route
-          path="/get-in-touch"
-          element={<ContactUs />}
-        />
-        <Route
-          path="/en/terms-and-conditions"
-          element={<TermsAndConditionsEn />}
-        />
-        <Route
-          path="/de/terms-and-conditions"
-          element={<TermsAndConditionsDe />}
-        />
-        <Route
-          path="/en/imprint"
-          element={<ImprintEn />}
-        />
-        <Route
-          path="/de/imprint"
-          element={<ImprintDe />}
-        />
-        <Route
-          path="/en/privacy-policy"
-          element={<PrivacyPolicyEn />}
-        />
-         <Route
-          path="/privacypolicy"
-          element={<PrivacyPolicyEn />}
-        />
-        <Route
-          path="/de/privacy-policy"
-          element={<PrivacyPolicyDe />}
-        />
-      </Routes>
-      <Footer />
-    </Router>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={<HeroSection />}
+          />
+          <Route
+            path="/pricing"
+            element={<PricingTable />}
+          />
+          <Route
+            path="/signup"
+            element={<SignUpForm />}
+          />
+          <Route
+            path="/faq"
+            element={<Help />}
+          />
+          <Route
+            path="/why-fotogram"
+            element={<HowItWorks />}
+          />
+          <Route
+            path="/our-story"
+            element={<WhoWeAre />}
+          />
+          <Route
+            path="/get-in-touch"
+            element={<ContactUs />}
+          />
+          <Route
+            path="/en/terms-and-conditions"
+            element={<TermsAndConditionsEn />}
+          />
+          <Route
+            path="/de/terms-and-conditions"
+            element={<TermsAndConditionsDe />}
+          />
+          <Route
+            path="/en/imprint"
+            element={<ImprintEn />}
+          />
+          <Route
+            path="/de/imprint"
+            element={<ImprintDe />}
+          />
+          <Route
+            path="/en/privacy-policy"
+            element={<PrivacyPolicyEn />}
+          />
+          <Route
+            path="/de/privacy-policy"
+            element={<PrivacyPolicyDe />}
+          />
+        </Routes>
+        <Footer />
+      </Router>
+    </HelmetProvider>
   );
 }
 
--- fotogram/src/components/ScrollToTop.js
+++ fotogram/src/components/ScrollToTop.js
@@ -3,15 +3,71 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import translate from '../helpers/translate';
 
-const ScrollToTop = ({ titles }) => {
+const updateOpenGraphTags = (title, description, url) => {
+  const ogTags = [
+    { property: 'og:title', content: title },
+    { property: 'og:description', content: description },
+    { property: 'og:url', content: url },
+    { property: 'og:type', content: 'website' },
+  ];
+
+  ogTags.forEach(({ property, content }) => {
+    let tag = document.querySelector(`meta[property="${property}"]`);
+    if (tag) {
+      tag.content = content;
+    } else {
+      tag = document.createElement('meta');
+      tag.setAttribute('property', property);
+      tag.content = content;
+      document.head.appendChild(tag);
+    }
+  });
+};
+
+const ScrollToTop = ({
+  titles,
+  metaDescriptions,
+  defaultTitle = 'Fotogram Technology',
+  defaultDescription = '',
+}) => {
   const { pathname } = useLocation();
 
   useEffect(() => {
     document.getElementById('root').scrollTo(0, 0);
 
-    const newTitle = translate(titles[pathname]) || 'Fotogram Technology';
+    // Translate titles and descriptions
+    const titleData = titles[pathname];
+    const descriptionData = metaDescriptions[pathname];
+    const newTitle = titleData ? translate(titleData) : defaultTitle;
+    const newDescription = descriptionData
+      ? translate(descriptionData)
+      : defaultDescription;
+
     document.title = newTitle;
-  }, [pathname, titles]);
+
+    let metaDescriptionTag = document.querySelector('meta[name="description"]');
+    if (!metaDescriptionTag) {
+      metaDescriptionTag = document.createElement('meta');
+      metaDescriptionTag.name = 'description';
+      document.head.appendChild(metaDescriptionTag);
+    }
+    metaDescriptionTag.content = newDescription;
+
+    let canonicalTag = document.querySelector('link[rel="canonical"]');
+    if (!canonicalTag) {
+      canonicalTag = document.createElement('link');
+      canonicalTag.rel = 'canonical';
+      document.head.appendChild(canonicalTag);
+    }
+    canonicalTag.href = `${window.location.origin}${pathname}`;
+
+    // Update Open Graph tags
+    updateOpenGraphTags(
+      newTitle,
+      newDescription,
+      `${window.location.origin}${pathname}`
+    );
+  }, [pathname, titles, metaDescriptions, defaultTitle, defaultDescription]);
 
   return null;
 };
